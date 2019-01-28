e18965def08e80640a8d0d5ea7db94a7af29f6c8
NomadRiding <nomadriding@gmail.com>
2019-01-28 16:35:30 -0500

    fix typo

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -21,7 +21,7 @@ export default function(state = initialState, action) {
       return Object.assign({}, state, {
         isFetching: false,
         data: action.payload,
-        hasError: falses,
+        hasError: false,
         errorMessage: null,
       });
     case FETCHING_COIN_DATA_FAIL:
